d147dc61e8034833d637de90dcb05a4de481473f
Smyslova Natalia <madlynx@lenta.ru>
2022-10-07 15:50:33 +0300

    second commit

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,38 +1,34 @@
-import { heroes, orderOfHeroes } from "../app";
+import { heroes, orderOfHeroes } from '../app';
 
 // ПЕРВЫЙ ТЕСТ
 
 test.each([
-   { name: "Маг", health: 100, expected: "healthy" },
-   { name: "Лучник", health: 80, expected: "wounded" },
-   { name: "Мечник", health: 10, expected: "critical" }
-])("testing level of health", ({ name, health, expected }) => {
+  { name: 'Маг', health: 100, expected: 'healthy' },
+  { name: 'Лучник', health: 80, expected: 'wounded' },
+  { name: 'Мечник', health: 10, expected: 'critical' },
+])('testing level of health', ({ name, health, expected }) => {
+  const result = heroes({ name, health });
 
-   const result = heroes({ name: name, health: health });
-   console.log(result);
-
-   expect(result).toBe(expected);
+  expect(result).toBe(expected);
 });
 
 // ВТОРОЙ ТЕСТ
 
-let list = [
-   { name: 'мечник', health: 10 },
-   { name: 'маг', health: 100 },
-   { name: 'лучник', health: 80 },
-   { name: 'вор', health: 80 },
+const list = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'вор', health: 80 },
 ];
 
-test("testing order of health", () => {
-   const newOrder = orderOfHeroes(list);
-   let result = [];
+test('testing order of health', () => {
+  const newOrder = orderOfHeroes(list);
+  const result = [];
 
-   newOrder.forEach(function (item, i, newOrder) {
-      result.push(item.health);
-   });
+  newOrder.forEach((item) => {
+    result.push(item.health);
+  });
 
-   const expected = [100, 80, 80, 10];
-   expect(result).toEqual(expected);
+  const expected = [100, 80, 80, 10];
+  expect(result).toEqual(expected);
 });
-
-
--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,27 @@
 // TODO: write your code here
 
-export function heroes({ name: name, health: health }) {
-    //условная конструкция
-    if (health === 100) {
-        return "healthy"
-    }
-    if (health < 100 && health > 15) {
-        return "wounded"
-    }
-    else return "critical"
-
-};
-
+export function heroes({ health }) {
+  // условная конструкция
+  if (health === 100) {
+    return 'healthy';
+  }
+  if (health < 100 && health > 15) {
+    return 'wounded';
+  }
+  return 'critical';
+}
 
 
 export function orderOfHeroes(heroesList) {
-    let result = heroesList.sort(function (a, b) {
-        if (a.health > b.health) {
-            return -1;
-        }
-        if (a.health < b.health) {
-            return 1;
-        }
-        // a должно быть равным b
-        return 0 ;
-    })
-    return result
-};
+  const result = heroesList.sort((a, b) => {
+    if (a.health > b.health) {
+      return -1;
+    }
+    if (a.health < b.health) {
+      return 1;
+    }
+    // a должно быть равным b
+    return 0;
+  });
+  return result;
+}
